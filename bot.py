ea944742f060900d73c19bf2f5095b618df63233
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-11-13 23:50:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,12 +44,6 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
-      app = web.AppRunner(await web_server())
-      await app.setup()
-      bind_address = "0.0.0.0"
-      await web.TCPSite(app, bind_address, PORT).start()
-      logging.info(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
-        
         
         
     async def stop(self, *args):
